31f315410e4d0ef7b207f1c00f95c8246f82fce8
zzacong <ongzz100@gmail.com>
2020-12-31 11:59:16 +1100

    client use port 3000

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const deps = require('./package.json').dependencies
 module.exports = {
   output: {
-    publicPath: 'http://localhost:8080/',
+    publicPath: 'http://localhost:3000/',
   },
 
   resolve: {
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 8080,
+    port: 3000,
   },
 
   module: {
